62073213c6e1bc32aef0413458f09099ffc4e2e9
jogeswar106 <jogeshwarpanigrahi@gmail.com>
2021-10-10 22:06:23 +0530

    searching

---
+++ Function_pre.cpp
@@ -0,0 +1,15 @@
+// Function  type Taking nothing and return nothing .
+
+#include <iostream>
+#include <string>
+using namespace std;
+void fun (void);
+int main()
+{
+    fun();
+    return 0;
+}
+void fun (void)
+{
+    cout << "hello yar\n  ";
+}
